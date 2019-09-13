508cd300d36bd1b3b5e112fac1aa2e29d9b68b0d
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-13 23:35:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,66 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort_by do |word|
+    word.length
+  end 
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+ 
+def reverse_array(array)
+  array.reverse 
+end
+
+#kesha_maker 
+
+def kesha_maker(array)
+  array.each do |kesh|
+    kesh[2] = "$"
+  end 
+end
+
+#find_a method
+
+def find_a(array)
+  array.select do |wordsA|
+    wordsA.start_with?("a") do |puta|
+    puta
+  end
+  end
+end
+
+#sum_array method
+
+def sum_array(array)
+  sum = 0 
+  array.each do |num|
+    sum += num 
+  end 
+  sum
+end 
+
+#add_s method
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end 
+  end 
+end
+
+      
+  
+  
